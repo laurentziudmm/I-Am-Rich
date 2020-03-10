c410dceeee0f3ba3eec2d71152e22fa551a6cf09
laurentziudmm <laurentziudmm@gmail.com>
2020-03-10 17:31:03 +0200

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Stoica Laurentiu on 10/03/2020.
+//  Copyright © 2020 Stoica Laurentiu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
